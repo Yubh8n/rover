3ce8e0c6dafc85bf419298de1a166460823de6d6
Chris M <yubh8n.sdu@gmail.com>
2026-07-15 20:17:42 +0100

    made a publish to arduino with 10 hz, to make sure that the motors set the desired speed.

--- ros_ws/src/arduino_com/arduino_com/com.py
+++ ros_ws/src/arduino_com/arduino_com/com.py
@@ -1,24 +1,36 @@
 import serial
-import threading
 import time
-import std_msgs
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int8
+from std_msgs.msg import UInt8
 
 class arduino_com(Node):
     def __init__(self):
         super().__init__('arduino_com')
-        self.sub = self.create_subscription(Int8, 'motor_speed', self.motor_callback, 1)
+        self.sub = self.create_subscription(UInt8, 'motor_speed', self.motor_callback, 1)
         self.ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
         time.sleep(2)
         self.ser.reset_input_buffer()
+        self.timer = self.create_timer(0.1, self.test)
+        self.L = 0
+        self.R = 0
+        self.response_gotten = True
+        print("Arduino com ready. ")
 
-    #FIT0441 (12VDC BLDC)#
+    # FIT0441 (12VDC BLDC) #
     def motor_callback(self, data):
+        self.response_gotten = False
+        self.L = data.data
+        self.R = data.data
         print(f'Sending: {data.data} to arduino')
-        self.ser.write(f"V {data.data} -100\n".encode())
 
+    def test(self):
+        if self.ser.in_waiting > 0:
+            line = self.ser.readline().decode('utf-8').rstrip()
+            self.get_logger().info(line)
+            self.response_gotten = True
+        if not self.response_gotten:
+            self.ser.write(f"V {self.L} {self.R}\n".encode())
 
 def main() -> None:
     rclpy.init()
@@ -29,7 +41,6 @@ def main() -> None:
         pass
     rclpy.shutdown()
 
-    # ser.write(f"V 0 -100\n".encode())
     # while True:
     #     if ser.in_waiting > 0:
     #         line = ser.readline().decode('utf-8').rstrip()
